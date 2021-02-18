21377e24712a73b746ba2fa1622d430a26fe561f
Carter Venne <you@example.com>
2021-02-18 11:56:29 -0600

    setup inquirer prompt

---
+++ index.js
@@ -0,0 +1,51 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+inquirer.prompt([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'Enter your applications name:'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Enter a brief summary of your application:'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Enter your installation instructions before use:' 
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Enter your instructions for use:' 
+    },
+    {
+        type: 'input',
+        name: 'contribute',
+        message: 'Enter your guidelines for future contributors:' 
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Enter tests built for this application:' 
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Please input your github account:' 
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Please input your email:' 
+    },
+    {
+        type: 'checkbox',
+        name: 'license',
+        message: 'Which licence should be attached to your apllication?',
+        choices: ['GNU', 'Apache', 'PERL', 'IBM', 'EPL', 'MIT']
+    },
+])
